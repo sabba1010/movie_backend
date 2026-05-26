b1d1a781d0b5adc9e64897d7850249c04ae461fa
Sabba Hossain <sabbahossain@gmail.com>
2026-05-26 09:59:00 +0600

    addd vercel

--- src/index.js
+++ src/index.js
@@ -60,6 +60,10 @@ app.use((err, req, res, next) => {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+if (!process.env.VERCEL) {
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });
+}
+
+module.exports = app;
